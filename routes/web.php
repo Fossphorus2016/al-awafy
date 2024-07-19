241f256b04846c868fc7826dfc8cb9d4269b2c64
FarazPeerzada <162085198+FarazPeerzada@users.noreply.github.com>
2024-07-19 12:04:09 +0500

    views according to languages

--- routes/web.php
+++ routes/web.php
@@ -5,12 +5,51 @@ use App\Http\Controllers\FormController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
+
+
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('en.home');
 });
 
-Route::post('/contact/create',[FormController::class,'contact_create'])->name('contact.create');
-Route::post('/news-letter/create',[FormController::class,'news_letter_create'])->name('news.letter.create');
+
+
+Route::prefix('en')->group(function () {
+    Route::get('/about', function () {return view('en.about');})->name('en.about');
+    Route::get('/home', function () {return view('en.home');})->name('en.home');
+    Route::get('/contact', function () {return view('en.contact_us');})->name('en.contact');
+    Route::get('/our-products', function () {return view('en.our_products');})->name('en.our.products');
+    Route::get('/blogs', function () {return view('en.blogs');})->name('en.blogs');
+
+});
+
+
+Route::prefix('ar')->group(function () {
+    Route::get('/about', function () {return view('ar.about');})->name('ar.about');
+    Route::get('/home', function () {return view('ar.home');})->name('ar.home');
+    Route::get('/contact', function () {return view('ar.contact_us');})->name('ar.contact');
+    Route::get('/our-products', function () {return view('ar.our_products');})->name('ar.our.products');
+    Route::get('/blogs', function () {return view('ar.blogs');})->name('ar.blogs');
+
+});
+
+
+Route::prefix('fr')->group(function () {
+    Route::get('/about', function () {return view('fr.about');})->name('fr.about');
+    Route::get('/home', function () {return view('fr.home');})->name('fr.home');
+    Route::get('/contact', function () {return view('fr.contact_us');})->name('fr.contact');
+    Route::get('/our-products', function () {return view('fr.our_products');})->name('fr.our.products');
+    Route::get('/blogs', function () {return view('fr.blogs');})->name('fr.blogs');
+
+});
+
+
+
+
+
+
+
+Route::post('/contact/create', [FormController::class, 'contact_create'])->name('contact.create');
+Route::post('/news-letter/create', [FormController::class, 'news_letter_create'])->name('news.letter.create');
 
 Auth::routes();
 
@@ -21,6 +60,4 @@ Route::group(['middleware' => 'auth'], function () {
 
     Route::get('/admin-news-letter', [AdminController::class, 'get_news_letter'])->name('news.letter');
     Route::delete('/admin/news-letter-delete/{id}', [AdminController::class, 'news_letter_delete'])->name('news.letter.delete');
-
-
 });
