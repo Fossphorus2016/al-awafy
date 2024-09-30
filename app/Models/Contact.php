95972e86a9e3609d8ad7ecad1e36e3ec06cd44ab
FarazPeerzada <162085198+FarazPeerzada@users.noreply.github.com>
2024-09-30 17:47:01 +0500

    2133

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -4,9 +4,57 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Crypt;
 
 class Contact extends Model
 {
     use HasFactory;
-    protected $fillable = ['name', 'phone', 'email','message'];
+    protected $fillable = ['name', 'phone', 'email', 'message'];
+
+    public function setNameAttribute($value)
+    {
+        $this->attributes['name'] = Crypt::encryptString($value);
+    }
+
+    // Accessor for decrypting the name
+    public function getNameAttribute($value)
+    {
+        return Crypt::decryptString($value);
+    }
+
+    // Mutator for encrypting the phone
+    public function setPhoneAttribute($value)
+    {
+        $this->attributes['phone'] = Crypt::encryptString($value);
+    }
+
+    // Accessor for decrypting the phone
+    public function getPhoneAttribute($value)
+    {
+        return Crypt::decryptString($value);
+    }
+
+    // Mutator for encrypting the email
+    public function setEmailAttribute($value)
+    {
+        $this->attributes['email'] = Crypt::encryptString($value);
+    }
+
+    // Accessor for decrypting the email
+    public function getEmailAttribute($value)
+    {
+        return Crypt::decryptString($value);
+    }
+
+    // Mutator for encrypting the message
+    public function setMessageAttribute($value)
+    {
+        $this->attributes['message'] = Crypt::encryptString($value);
+    }
+
+    // Accessor for decrypting the message
+    public function getMessageAttribute($value)
+    {
+        return Crypt::decryptString($value);
+    }
 }
--- resources/views/en/contact_us.blade.php
+++ resources/views/en/contact_us.blade.php
@@ -294,9 +294,13 @@
                                         <small id="error_message" class="text-danger position-absolute"
                                             style="display: none; bottom: -20px;">Message is required</small>
                                     </div>
+
+
                                     <div class="col-12 d-flex justify-content-center">
                                         <button class="mainBtn1 w-100" type="submit">SUBMIT NOW!</button>
                                     </div>
+
+                                    <x-captcha />
                                 </div>
                             </form>
                         </div>
@@ -344,7 +348,9 @@
         <section class="mapSection">
 
 
-            <iframe class=" mapIframe secondValues" src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3220.6003876465056!2d5.413036175809924!3d36.17627777243146!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMzbCsDEwJzM0LjYiTiA1wrAyNCc1Ni4yIkU!5e0!3m2!1sen!2s!4v1726751132615!5m2!1sen!2s"allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <iframe class=" mapIframe secondValues"
+                src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3220.6003876465056!2d5.413036175809924!3d36.17627777243146!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMzbCsDEwJzM0LjYiTiA1wrAyNCc1Ni4yIkU!5e0!3m2!1sen!2s!4v1726751132615!5m2!1sen!2s"allowfullscreen=""
+                loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 
 
 
@@ -359,7 +365,21 @@
 
     </main>
 
+
+
     <script>
+        let captchaToken = '';
+
+        function javascriptCallback(token) {
+            if (token) {
+                captchaToken = token;
+                console.log("Turnstile response token:", token);
+            } else {
+                console.log("Error: Turnstile token not generated.");
+                captchaToken = '';
+            }
+        }
+
         function contact_validate(event) {
             var isValid = true;
 
@@ -431,8 +451,18 @@
                 $("#error_message").hide();
             }
 
+            if (captchaToken) {
+                // Set the hidden captcha token field
+                $('#captcha_token').val(captchaToken);
+
+            } else {
+                $('#error_message').html("Please check the captcha").show();
+            }
+
+
+
             // If form is not valid, prevent submission
-            if (!isValid) {
+            if (!isValid && captchaToken) {
                 event.preventDefault();
             }
 
