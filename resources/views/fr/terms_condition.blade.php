4f9452ddb57d56443708372f01656e56a30158af
Burhan112c <bshaikh@fossphorus.com>
2024-10-02 18:58:55 +0500

    update

---
+++ resources/views/fr/terms_condition.blade.php
@@ -0,0 +1,203 @@
+<x-frenchLayout>
+    <main>
+        <section class="heroSection secondBanner contactBanner position-relative z-1">
+            <div class="customContainer">
+                <div class="heroSliderOuter">
+
+
+                    <div class="heroSliderIcon">
+                        <a class="icons fbLink" target="_blank" href=""> <i class="fa-brands fa-facebook-f"></i> </a>
+                        <a class="icons instaLink" target="_blank" href=""> <i
+                                class="fa-brands fa-instagram"></i></a>
+                        <a class="icons whatsappLink" target="_blank" href=""><i
+                                class="fa-brands fa-whatsapp"></i></a>
+                    </div>
+
+                    <div class="heroInner">
+                        <h1 class="waterDropsBefore waterDropsAfter waterDropsAfter3 waterDropsBefore3"> <span class="fs2">Conditions Générales  </span>
+                            <span class="fs3">d'Utilisation</span>
+                        </h1>
+
+                    </div>
+                </div>
+
+            </div>
+        </section>
+
+
+        <section>
+            <div class="container">
+
+                <div class="w-75 mt-5">
+
+                    <div class="">
+
+                        <p>
+                            Bienvenue sur le site web d'Alawafy. En accédant à notre site ou en l'utilisant, vous acceptez de vous
+                            conformer et d'être lié par les présentes conditions générales. Si vous n'acceptez pas une partie de
+                            ces conditions, veuillez ne pas utiliser notre site.
+
+                        </p>
+                    </div>
+                    <div class=" ">
+
+                        <ol>
+                            <li class="my-4">
+                                <strong class="fs-3">Acceptation des Conditions
+                                </strong>
+                                <p>
+                                    En utilisant notre site, vous confirmez que vous avez au moins 18 ans ou que vous utilisez le site sous
+                                    la supervision d'un parent ou d'un tuteur.
+
+                                </p>
+                            </li>
+                            <li class="my-4">
+                                <strong class="fs-3">
+                                    Modifications des Conditions
+                                </strong>
+                                <p>
+                                    Alawafy se réserve le droit de modifier ces conditions générales à tout moment. Les modifications
+                                    prendront effet immédiatement dès leur publication sur ce site. Votre utilisation continue du site
+                                    après toute modification signifie votre acceptation des nouvelles conditions.
+
+                                </p>
+                            </li>
+                            <li class="my-4">
+                                <strong class="fs-3"> Utilisation du Site:</strong>
+                                <p>
+                                    Vous vous engagez à utiliser le site uniquement à des fins légales et de manière à ne pas enfreindre
+                                    les droits d'autrui, ni à restreindre ou à inhiber l'utilisation du site par quiconque. Les activités
+                                    interdites incluent, mais ne se limitent pas à :
+
+                                <ul class="listStyles">
+                                    <li>Harcèlement ou causer du stress à quiconque</li>
+                                    <li>Transmission de contenus obscènes ou offensants
+
+                                    </li>
+                                    <li>Perturbation du flux normal de dialogue ou causant une perturbation</li>
+                                </ul>
+                                </p>
+                            </li>
+                            <li class="my-4">
+                                <strong class="fs-3">
+                                    Informations sur les Produits
+                                </strong>
+                                <p>
+                                    Nous nous efforçons de fournir des descriptions et des images précises des produits sur notre site.
+                                    Cependant, nous ne garantissons pas que les descriptions ou d'autres contenus soient exacts,
+                                    complets, fiables ou sans erreur. Si un produit proposé par Alawafy n'est pas conforme à sa
+                                    description, votre seul recours est de le retourner dans son état inutilisé.
+
+                                </p>
+                            </li>
+                            <li class="my-4">
+                                <strong class="fs-3">
+                                    Tarification et Disponibilité:
+                                </strong>
+                                <p>
+                                    Tous les prix sont susceptibles de changer sans préavis. Nous nous réservons le droit de modifier ou
+                                    de retirer des produits à tout moment. Nous ne sommes pas responsables envers vous ou un tiers de
+                                    toute modification, changement de prix, suspension ou retrait du service ou des produits.
+                                </p>
+                            </li>
+                            <li class="my-4">
+                                <strong class="fs-3">Commandes et Paiements:</strong>
+                                <p>
+                                    Lorsque vous passez une commande via notre site, vous acceptez de fournir des informations exactes,
+                                    actuelles et complètes. Tous les paiements sont traités de manière sécurisée. En soumettant votre
+                                    commande, vous acceptez de payer le prix indiqué et toutes taxes et frais d'expédition applicables.
+
+                                </p>
+                            </li>
+                            <li class="my-4">
+                                <strong class="fs-3">
+                                    Retours et Remboursements
+                                </strong>
+                                <p>
+                                    Veuillez vous référer à notre Politique de Retours pour des informations sur les retours et les
+                                    remboursements. Nous visons à fournir les meilleurs produits, et si vous n'êtes pas satisfait, nous
+                                    travaillerons avec vous pour résoudre tout problème.
+                                </p>
+                            </li>
+                            <li class="my-4">
+                                <strong class="fs-3">
+                                    Propriété Intellectuelle
+
+                                </strong>
+                                <p>
+                                    Tout le contenu de ce site, y compris les textes, graphiques, logos et images, est la propriété
+                                    d'Alawafy et est protégé par des lois sur le droit d'auteur et la propriété intellectuelle. L'utilisation
+                                    non autorisée de tout contenu peut violer les lois sur le droit d'auteur, la marque et d'autres lois.
+
+                                </p>
+                            </li>
+                            <li class="my-4">
+                                <strong class="fs-3">
+                                    Limitation de Responsabilité
+
+                                </strong>
+                                <p>
+                                    Alawafy ne saurait être tenu responsable de tout dommage direct, indirect, accessoire, consécutif ou
+                                    punitif résultant de votre utilisation du site ou de tout produit acheté via celui-ci. Votre seul recours
+                                    en cas d'insatisfaction concernant le site ou tout produit est de cesser d'utiliser le site.
+
+                                </p>
+                            </li>
+                            <li class="my-4">
+                                <strong class="fs-3">
+                                    Droit Applicable
+                                </strong>
+                                <p>
+                                    Ces conditions générales sont régies par et interprétées conformément aux lois de l'Algérie. Tout
+                                    litige découlant de ces conditions sera résolu devant les tribunaux d'Algérie.
+
+
+                                </p>
+                            </li>
+                            <li class="my-4">
+                                <strong class="fs-3">
+                                    Contactez-nous
+                                </strong>
+                                <p>
+                                    Si vous avez des questions concernant ces Conditions Générales d'Utilisation, veuillez nous contacter
+                                    à :
+
+                                    <ul class="listStyles inlineContent">
+                                        <li> <strong>E-mail ::</strong>
+                                            <p>info@Alawafy.com</p>
+                                        </li>
+                                        <li>
+                                            <strong>Téléphone:</strong>
+                                            <p>+213-557-666-888</p>
+                                        </li>
+                                        <li>
+                                            <strong>Adresse</strong>
+                                            <p>ZEA 1 ère Tranche N°06 -Sétif- Algeria</p>
+                                        </li>
+                                    </ul>
+                                </p>
+                            </li>
+
+
+                        </ol>
+                    </div>
+                    <p>
+                        En utilisant ce site, vous reconnaissez avoir lu, compris et accepté d'être lié par ces Conditions
+                        Générales d'Utilisation.
+                    </p>
+                </div>
+
+            </div>
+        </section>
+
+
+
+
+        <x-letsConnect />
+
+
+
+
+    </main>
+
+</x-frenchLayout>
