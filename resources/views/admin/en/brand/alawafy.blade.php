b83f983d8ca33f2d3a5fec877ae9c78af3d6c835
Burhan112c <bshaikh@fossphorus.com>
2024-10-21 19:06:52 +0500

    update

--- resources/views/admin/en/brand/alawafy.blade.php
+++ resources/views/admin/en/brand/alawafy.blade.php
@@ -303,7 +303,7 @@
                                             data-bs-parent="#accordionBrand">
                                             <div class="accordion-body">
                                                 <form action="{{ route('alawafy.item.2') }}" method="POST"
-                                                    enctype="multipart/form-data" class="mt-5 formValidation2">
+                                                    enctype="multipart/form-data" class="mt-5 formValidation5">
                                                     @csrf
                                                     <input type="hidden" name="language" value="english">
                                                     <div class="row gy-4">
@@ -389,7 +389,7 @@
                                             data-bs-parent="#accordionBrand">
                                             <div class="accordion-body">
                                                 <form action="{{ route('alawafy.item.3') }}" method="POST"
-                                                    enctype="multipart/form-data" class="mt-5 formValidation2">
+                                                    enctype="multipart/form-data" class="mt-5 formValidation6">
                                                     @csrf
                                                     <input type="hidden" name="language" value="english">
                                                     <div class="row gy-4">
@@ -475,7 +475,7 @@
                                             data-bs-parent="#accordionBrand">
                                             <div class="accordion-body">
                                                 <form action="{{ route('alawafy.item.4') }}" method="POST"
-                                                    enctype="multipart/form-data" class="mt-5 formValidation2">
+                                                    enctype="multipart/form-data" class="mt-5 formValidation7">
                                                     @csrf
                                                     <input type="hidden" name="language" value="english">
                                                     <div class="row gy-4">
@@ -561,7 +561,7 @@
                                             data-bs-parent="#accordionBrand">
                                             <div class="accordion-body">
                                                 <form action="{{ route('alawafy.item.5') }}" method="POST"
-                                                    enctype="multipart/form-data" class="mt-5 formValidation2">
+                                                    enctype="multipart/form-data" class="mt-5 formValidation8">
                                                     @csrf
                                                     <input type="hidden" name="language" value="english">
                                                     <div class="row gy-4">
@@ -647,7 +647,7 @@
                                             data-bs-parent="#accordionBrand">
                                             <div class="accordion-body">
                                                 <form action="{{ route('alawafy.item.6') }}" method="POST"
-                                                    enctype="multipart/form-data" class="mt-5 formValidation2">
+                                                    enctype="multipart/form-data" class="mt-5 formValidation9">
                                                     @csrf
                                                     <input type="hidden" name="language" value="english">
                                                     <div class="row gy-4">
@@ -733,7 +733,7 @@
                                             data-bs-parent="#accordionBrand">
                                             <div class="accordion-body">
                                                 <form action="{{ route('alawafy.item.7') }}" method="POST"
-                                                    enctype="multipart/form-data" class="mt-5 formValidation2">
+                                                    enctype="multipart/form-data" class="mt-5 formValidation10">
                                                     @csrf
                                                     <input type="hidden" name="language" value="english">
                                                     <div class="row gy-4">
@@ -925,7 +925,11 @@
                 }
             });
 
-            function validationItems(input, errMsg) {
+            function validationItems(input, errMsg){
+
+            }
+
+            $('.formValidation4').submit(function(event) {
                 event.preventDefault();
 
                 let valid = true;
@@ -934,7 +938,7 @@
 
                 if ($(".editorOut930 input").val() == "") {
                     $(".editorOut930 input").addClass("is-invalid");
-                    $(".editorOut930 .errMsg").text('Heading is required');
+                    $(".editorOut930 .errMsg").text('Content is required');
                     valid = false
                 } else {
                     $(".editorOut930 input").removeClass("is-invalid");
@@ -942,31 +946,276 @@
 
                 if ($(".editorOut94 input").val() == "") {
                     $(".editorOut94 input").addClass("is-invalid");
-                    $(".editorOut94 .errMsg").text('Heading is required');
+                    $(".editorOut94 .errMsg").text('Content is required');
                     valid = false
                 } else {
                     $(".editorOut94 input").removeClass("is-invalid");
                 }
                 if ($(".editorOut96 input").val() == "") {
                     $(".editorOut96 input").addClass("is-invalid");
-                    $(".editorOut96 .errMsg").text('Heading is required');
+                    $(".editorOut96 .errMsg").text('Content is required');
                     valid = false
                 } else {
                     $(".editorOut96 input").removeClass("is-invalid");
                 }
 
                 if (!brand_1_p.value.trim() || brand_1_p.value.trim() === '<p><br></p>') {
-                    $('.editorOut95 .errMsg').text('Paragraph 1 is required.');
+                    $('.editorOut95 .errMsg').text('Paragraph is required.');
                     valid = false;
                 }
 
                 if (valid) {
                     this.submit();
                 }
-            }
+            });
+            $('.formValidation5').submit(function(event) {
+                event.preventDefault();
 
-            $('.formValidation4').submit(function(event) {
-                validationItems()
+                let valid = true;
+
+                $('.errMsg').text('');
+
+                if ($(".editorOut97 input").val() == "") {
+                    $(".editorOut97 input").addClass("is-invalid");
+                    $(".editorOut97 .errMsg").text('Content is required');
+                    valid = false
+                } else {
+                    $(".editorOut97 input").removeClass("is-invalid");
+                }
+
+                if ($(".editorOut98 input").val() == "") {
+                    $(".editorOut98 input").addClass("is-invalid");
+                    $(".editorOut98 .errMsg").text('Content is required');
+                    valid = false
+                } else {
+                    $(".editorOut98 input").removeClass("is-invalid");
+                }
+                if ($(".editorOut11 input").val() == "") {
+                    $(".editorOut11 input").addClass("is-invalid");
+                    $(".editorOut11 .errMsg").text('Content is required');
+                    valid = false
+                } else {
+                    $(".editorOut11 input").removeClass("is-invalid");
+                }
+
+                if (!brand_2_p.value.trim() || brand_2_p.value.trim() === '<p><br></p>') {
+                    $('.editorOut99 .errMsg').text('Paragraph is required.');
+                    valid = false;
+                }
+
+                if (valid) {
+                    this.submit();
+                }
+            });
+            $('.formValidation6').submit(function(event) {
+                event.preventDefault();
+
+                let valid = true;
+
+                $('.errMsg').text('');
+
+                if ($(".editorOut12 input").val() == "") {
+                    $(".editorOut12 input").addClass("is-invalid");
+                    $(".editorOut12 .errMsg").text('Content is required');
+                    valid = false
+                } else {
+                    $(".editorOut12 input").removeClass("is-invalid");
+                }
+
+                if ($(".editorOut13 input").val() == "") {
+                    $(".editorOut13 input").addClass("is-invalid");
+                    $(".editorOut13 .errMsg").text('Content is required');
+                    valid = false
+                } else {
+                    $(".editorOut13 input").removeClass("is-invalid");
+                }
+                if ($(".editorOut15 input").val() == "") {
+                    $(".editorOut15 input").addClass("is-invalid");
+                    $(".editorOut15 .errMsg").text('Content is required');
+                    valid = false
+                } else {
+                    $(".editorOut15 input").removeClass("is-invalid");
+                }
+                console.log('worr')
+
+                if (!brand_3_p.value.trim() || brand_3_p.value.trim() === '<p><br></p>') {
+                console.log('workinggg')
+
+                    $('.editorOut14 .errMsg').text('Paragraph is required.');
+                    valid = false;
+                }
+
+                if (valid) {
+                    this.submit();
+                }
+            });
+            $('.formValidation7').submit(function(event) {
+                event.preventDefault();
+
+                let valid = true;
+
+                $('.errMsg').text('');
+
+                if ($(".editorOut16 input").val() == "") {
+                    $(".editorOut16 input").addClass("is-invalid");
+                    $(".editorOut16 .errMsg").text('Content is required');
+                    valid = false
+                } else {
+                    $(".editorOut16 input").removeClass("is-invalid");
+                }
+
+                if ($(".editorOut17 input").val() == "") {
+                    $(".editorOut17 input").addClass("is-invalid");
+                    $(".editorOut17 .errMsg").text('Content is required');
+                    valid = false
+                } else {
+                    $(".editorOut17 input").removeClass("is-invalid");
+                }
+                if ($(".editorOut19 input").val() == "") {
+                    $(".editorOut19 input").addClass("is-invalid");
+                    $(".editorOut19 .errMsg").text('Content is required');
+                    valid = false
+                } else {
+                    $(".editorOut19 input").removeClass("is-invalid");
+                }
+
+
+                if (!brand_4_p.value.trim() || brand_4_p.value.trim() === '<p><br></p>') {
+
+
+                    $('.editorOut18 .errMsg').text('Paragraph is required.');
+                    valid = false;
+                }
+
+                if (valid) {
+                    this.submit();
+                }
+            });
+            $('.formValidation8').submit(function(event) {
+                event.preventDefault();
+
+                let valid = true;
+
+                $('.errMsg').text('');
+
+                if ($(".editorOut20 input").val() == "") {
+                    $(".editorOut20 input").addClass("is-invalid");
+                    $(".editorOut20 .errMsg").text('Content is required');
+                    valid = false
+                } else {
+                    $(".editorOut20 input").removeClass("is-invalid");
+                }
+
+                if ($(".editorOut21 input").val() == "") {
+                    $(".editorOut21 input").addClass("is-invalid");
+                    $(".editorOut21 .errMsg").text('Content is required');
+                    valid = false
+                } else {
+                    $(".editorOut21 input").removeClass("is-invalid");
+                }
+                if ($(".editorOut23 input").val() == "") {
+                    $(".editorOut23 input").addClass("is-invalid");
+                    $(".editorOut23 .errMsg").text('Content is required');
+                    valid = false
+                } else {
+                    $(".editorOut23 input").removeClass("is-invalid");
+                }
+
+
+                if (!brand_5_p.value.trim() || brand_5_p.value.trim() === '<p><br></p>') {
+
+
+                    $('.editorOut22 .errMsg').text('Paragraph is required.');
+                    valid = false;
+                }
+
+                if (valid) {
+                    this.submit();
+                }
+            });
+            $('.formValidation9').submit(function(event) {
+                event.preventDefault();
+
+                let valid = true;
+
+                $('.errMsg').text('');
+
+                if ($(".editorOut24 input").val() == "") {
+                    $(".editorOut24 input").addClass("is-invalid");
+                    $(".editorOut24 .errMsg").text('Content is required');
+                    valid = false
+                } else {
+                    $(".editorOut24 input").removeClass("is-invalid");
+                }
+
+                if ($(".editorOut25 input").val() == "") {
+                    $(".editorOut25 input").addClass("is-invalid");
+                    $(".editorOut25 .errMsg").text('Content is required');
+                    valid = false
+                } else {
+                    $(".editorOut25 input").removeClass("is-invalid");
+                }
+                if ($(".editorOut27 input").val() == "") {
+                    $(".editorOut27 input").addClass("is-invalid");
+                    $(".editorOut27 .errMsg").text('Content is required');
+                    valid = false
+                } else {
+                    $(".editorOut27 input").removeClass("is-invalid");
+                }
+
+
+                if (!brand_6_p.value.trim() || brand_6_p.value.trim() === '<p><br></p>') {
+
+
+                    $('.editorOut26 .errMsg').text('Paragraph is required.');
+                    valid = false;
+                }
+
+                if (valid) {
+                    this.submit();
+                }
+            });
+            $('.formValidation10').submit(function(event) {
+                event.preventDefault();
+
+                let valid = true;
+
+                $('.errMsg').text('');
+
+                if ($(".editorOut28 input").val() == "") {
+                    $(".editorOut28 input").addClass("is-invalid");
+                    $(".editorOut28 .errMsg").text('Content is required');
+                    valid = false
+                } else {
+                    $(".editorOut28 input").removeClass("is-invalid");
+                }
+
+                if ($(".editorOut29 input").val() == "") {
+                    $(".editorOut29 input").addClass("is-invalid");
+                    $(".editorOut29 .errMsg").text('Content is required');
+                    valid = false
+                } else {
+                    $(".editorOut29 input").removeClass("is-invalid");
+                }
+                if ($(".editorOut31 input").val() == "") {
+                    $(".editorOut31 input").addClass("is-invalid");
+                    $(".editorOut31 .errMsg").text('Content is required');
+                    valid = false
+                } else {
+                    $(".editorOut31 input").removeClass("is-invalid");
+                }
+
+
+                if (!brand_7_p.value.trim() || brand_7_p.value.trim() === '<p><br></p>') {
+
+
+                    $('.editorOut30 .errMsg').text('Paragraph is required.');
+                    valid = false;
+                }
+
+                if (valid) {
+                    this.submit();
+                }
             });
 
 
