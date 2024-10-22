5f17e63bb6e5d7f9aceb14eca7443063b5f5fa59
Burhan112c <bshaikh@fossphorus.com>
2024-10-22 12:27:33 +0500

    update

---
+++ resources/views/admin/en/our_activity.blade.php
@@ -0,0 +1,290 @@
+<x-admin.layouts>
+    <div class="card">
+        <div class="card-body">
+            <div class="accordion" id="accordionExample">
+
+                <div class="accordion-item">
+                    <h2 class="accordion-header">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#collapseZero" aria-expanded="true" aria-controls="collapseZero">
+                            Meta Section
+                        </button>
+                    </h2>
+                    <div id="collapseZero" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                            <form action="" method="POST" class="mt-5 formValidation">
+                                @csrf
+                                <input type="hidden" name="language" value="english">
+                                <div class="row gy-4">
+                                    <div class="col-6">
+
+                                        <div>
+                                            <label for="">Meta title</label>
+                                            <input class="form-control" name="meta_title"
+                                                value=""></input>
+                                            <p class="errMsg text-danger"></p>
+                                        </div>
+                                    </div>
+                                    <div class="col-6">
+
+                                        <div>
+                                            <label for="">Meta Description</label>
+                                            <input class="form-control" name="meta_description"
+                                                value=""></input>
+                                            <p class="errMsg text-danger"></p>
+                                        </div>
+                                    </div>
+                                    <div class="col-6">
+                                        <div>
+                                            <label for="">Canonical</label>
+                                            <input class="form-control" name="canonical"
+                                                value=""></input>
+                                            <p class="errMsg text-danger"></p>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-12">
+                                        <button type="submit" class="btn btn-primary">Save</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="accordion-item">
+                    <h2 class="accordion-header">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#collapseZero2" aria-expanded="false" aria-controls="collapseZero2">
+                            Banner Section
+                        </button>
+                    </h2>
+                    <div id="collapseZero2" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                            <div>
+                                <form action="" method="POST"
+                                    class="mt-5 formValidation" enctype="multipart/form-data">
+                                    @csrf
+                                    <input type="hidden" name="language" value="english">
+                                    <div class="row gy-4">
+                                        <div class="col-6">
+
+                                            <div class="editorOut12">
+                                                <label for="">Heading 1</label>
+                                                <input class="form-control" name="sec1_h1"
+                                                    value=""></input>
+                                                <p class="errMsg text-danger"></p>
+                                            </div>
+                                        </div>
+                                        <div class="col-6">
+
+                                            <div class="class="editorOut13>
+                                                <label for="">Heading 2</label>
+                                                <input class="form-control" name="sec1_h2"
+                                                    value=""></input>
+                                                <p class="errMsg text-danger"></p>
+                                            </div>
+                                        </div>
+                                        <div class="col-6">
+                                            <div>
+                                                <label for="banner_1_image">Banner Image</label>
+                                                <input type="file" class="form-control " name="sec1_image"
+                                                    id="bannerImageInput1">
+
+                                                <p class="text-danger fileError"></p>
+                                            </div>
+                                        </div>
+
+                                        <div class="col-12">
+                                            <img  alt="" id="uploadedImage1" style="background-color:lightgrey; max-width: 1600px; width: 100%; height: 460px; object-fit: contain; ">
+                                        </div>
+                                        <div class="col-12">
+                                            <button type="submit" class="btn btn-primary">Save</button>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+    </div>
+
+    <script>
+        $(document).ready(function() {
+
+            // const editor1 = new Jodit('.editor1');
+
+
+
+            $(".formValidation").submit(function(event) {
+                event.preventDefault();
+                let isValid = true;
+
+                $(this)
+                    .find("input")
+                    .each(function() {
+                        let $inputField = $(this);
+                        let $parentDiv = $inputField.closest('div');
+                        let $errorMessage = $parentDiv.find('.errMsg');
+                        let labelText = $parentDiv.find('label').text();
+
+                        if ($inputField.hasClass("noVal")) {
+                            $inputField.removeClass("is-invalid");
+                            return;
+                        }
+                        if ($(this).is(":text")) {
+                            if ($inputField.val().trim() === "") {
+                                $inputField.addClass("is-invalid");
+                                $errorMessage.text(labelText + " is required").css('display', 'block');
+                                isValid = false;
+                            } else {
+                                $inputField.removeClass("is-invalid");
+                                $errorMessage.text('').css('display', 'none');
+                            }
+                        }
+
+                    });
+                if (isValid) {
+                    this.submit();
+                }
+            });
+
+            $('.formValidation2').submit(function(event) {
+                event.preventDefault();
+
+                let valid = true;
+
+                $('.errMsg').text('');
+
+                if (!editor1.value.trim() || editor1.value.trim() === '<p><br></p>') {
+                    $('.editorOut1 .errMsg').text('Paragraph 1 is required.');
+                    valid = false;
+                    console.log(editor1.value, 'ed1');
+                }
+
+                if (!editor2.value.trim() || editor2.value.trim() === '<p><br></p>') {
+                    $('.editorOut2 .errMsg').text('Paragraph 2 is required.');
+                    valid = false;
+                    console.log(editor2.value, 'ed2');
+                }
+
+                // if (!editor3.value.trim() || editor3.value.trim() === '<p><br></p>') {
+                //     $('.editorOut3 .errMsg').text('Paragraph 3 is required.');
+                //     valid = false;
+                //     console.log(editor3.value, 'ed3');
+                // }
+
+                // If all validations pass, submit the form
+                if (valid) {
+                    this.submit();
+                }
+            });
+            $('.formValidation3').submit(function(event) {
+                event.preventDefault();
+
+                let valid = true;
+
+                $('.errMsg').text('');
+
+
+                if ($(".editorOut4 input").val() == "") {
+                    $(".editorOut4 input").addClass("is-invalid");
+                    $(".editorOut4 .errMsg").text('Heading is required');
+                    valid = false
+                } else {
+                    $(".editorOut4 input").removeClass("is-invalid");
+                }
+
+                if ($(".editorOut5 input").val() == "") {
+                    $(".editorOut5 input").addClass("is-invalid");
+                    $(".editorOut5 .errMsg").text('Heading is required');
+                    valid = false
+                } else {
+                    $(".editorOut5 input").removeClass("is-invalid");
+                }
+                if ($(".editorOut8 input").val() == "") {
+                    $(".editorOut8 input").addClass("is-invalid");
+                    $(".editorOut8 .errMsg").text('Heading is required');
+                    valid = false
+                } else {
+                    $(".editorOut8 input").removeClass("is-invalid");
+                }
+                if ($(".editorOut9 input").val() == "") {
+                    $(".editorOut9 input").addClass("is-invalid");
+                    $(".editorOut9 .errMsg").text('Heading is required');
+                    valid = false
+                } else {
+                    $(".editorOut9 input").removeClass("is-invalid");
+                }
+                if ($(".editorOut10 input").val() == "") {
+                    $(".editorOut10 input").addClass("is-invalid");
+                    $(".editorOut10 .errMsg").text('Heading is required');
+                    valid = false
+                } else {
+                    $(".editorOut10 input").removeClass("is-invalid");
+                }
+                if ($(".editorOut11 input").val() == "") {
+                    $(".editorOut11 input").addClass("is-invalid");
+                    $(".editorOut11 .errMsg").text('Heading is required');
+                    valid = false
+                } else {
+                    $(".editorOut11 input").removeClass("is-invalid");
+                }
+                if ($(".editorOut12 input").val() == "") {
+                    $(".editorOut12 input").addClass("is-invalid");
+                    $(".editorOut12 .errMsg").text('Heading is required');
+                    valid = false
+                } else {
+                    $(".editorOut12 input").removeClass("is-invalid");
+                }
+                if ($(".editorOut13 input").val() == "") {
+                    $(".editorOut13 input").addClass("is-invalid");
+                    $(".editorOut13 .errMsg").text('Heading is required');
+                    valid = false
+                } else {
+                    $(".editorOut13 input").removeClass("is-invalid");
+                }
+
+                if (!editor4.value.trim() || editor4.value.trim() === '<p><br></p>') {
+                    $('.editorOut6 .errMsg').text('Paragraph 1 is required.');
+                    valid = false;
+                }
+
+                if (!editor5.value.trim() || editor5.value.trim() === '<p><br></p>') {
+                    $('.editorOut7 .errMsg').text('Paragraph 2 is required.');
+                    valid = false;
+                }
+
+
+                // If all validations pass, submit the form
+                if (valid) {
+                    this.submit();
+                }
+            });
+
+            function handleImageChange(inputId, imageId) {
+                $(inputId).on('change', function(event) {
+                    var file = event.target.files[0];
+                    var image = $(imageId);
+
+                    if (file) {
+                        var imageUrl = URL.createObjectURL(file);
+                        image.attr('src', imageUrl).show();
+                    } else {
+                        image.hide();
+                    }
+                });
+            }
+
+            handleImageChange('#bannerImageInput1', '#uploadedImage1');
+            handleImageChange('#imgS11', '#imgU11');
+            handleImageChange('#imgS12', '#imgU12');
+
+
+        });
+    </script>
+</x-admin.layouts>
