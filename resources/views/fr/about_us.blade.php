ac4df2d41c92459cf9414f8e337d047496cdf319
FarazPeerzada <162085198+FarazPeerzada@users.noreply.github.com>
2024-10-23 11:26:07 +0500

    Update about_us.blade.php

--- resources/views/fr/about_us.blade.php
+++ resources/views/fr/about_us.blade.php
@@ -1,10 +1,11 @@
 @php
+    $about = App\Models\About::where('language', 'french')->first();
 
 @endphp
 
 <x-frenchLayout>
     <main>
-        <section class="heroSection secondBanner" style="background-image: url('asset('storage/'.$)')">
+        <section class="heroSection secondBanner aboutBanner">
             <div class="customContainer">
                 <div class="heroSliderOuter">
 
@@ -18,8 +19,9 @@
                     </div>
 
                     <div class="heroInner">
-                        <h1 class="waterDropsBefore waterDropsAfter"> <span class="fs2"> A PROPOS  </span>
-                            <span class="fs3">DE NOUS</span>
+                        <h1 class="waterDropsBefore waterDropsAfter"> <span class="fs2"> {{ $about->sec1_h1 ?? '' }}
+                            </span>
+                            <span class="fs3">{{ $about->sec1_h2 ?? '' }}</span>
                         </h1>
 
                     </div>
@@ -33,21 +35,9 @@
                 <div class="row">
                     <div class="col-lg-6 col-12 d-flex align-item">
                         <div class="aboutInner paraFont">
-                            <h2 ><span class="fs5">a propos </span> <span class="fs6">d’Alawafy</span> </h2>
-                            <p>
-                                Fondée en 2023, ALAWFY est une société algérienne à responsabilité limitée (SARL)
-                                spécialisée dans la production de dattes et de nuts une entreprise dynamique située à
-                                Sétif en Algérie, Forts de notre engagement pour la qualité, nous mettons un point
-                                d'honneur à offrir des produits exceptionnels, aussi délicieux que sains.
-                            </p>
-                            <p>
-                                Grâce à l'utilisation de machine modernes répandant aux vigueurs et aux normes
-                                internationales, nous optimisons chaque étape de notre processus de production,
-                                garantissant des normes de qualité élevées et une innovation continue. Nos produits sont
-                                soigneusement élaborés pour satisfaire les goûts les plus exigeants, en alliant saveur
-                                et texture dans chaque bouchée.
-                            </p>
-
+                            <h2><span class="fs5">{{ $about->sec2_h1 ?? '' }}</span> <span
+                                    class="fs6">{{ $about->sec2_h2 ?? '' }}</span> </h2>
+                            {!! $about->sec2_p1 ?? '' !!}
 
                         </div>
                     </div>
@@ -68,47 +58,11 @@
                         </div>
                     </div>
                     <div class="col-12 paraFont mt-4 aboutThirdPara">
-                        <p>
-                            À ALAWAFY, nous croyons fermement que la qualité commence à la source. C'est pourquoi
-                            nous sélectionnons les meilleures noix et cacahuètes pour vous offrir des enrobages
-                            raffinés et savoureux, parfaits pour les moments de partage ou de plaisir personnel.
-                        </p>
+                        {!! $about->sec2_p2 ?? '' !!}
                     </div>
 
                     <div class="col-12  mt-4 paraFont inlineContent listStyles">
-                        <h2 class="fs12">Nos atouts :</h2>
-                        <ul class="my-4">
-                            <li>
-                                <strong>Une qualité irréprochable :</strong>
-                                <p class="">
-                                    des produits contrôlés et approuvés selon
-                                    les standards les
-                                    plus stricts.
-                                </p>
-                            </li>
-                            <li>
-                                <strong>Des technliogies avancées :</strong>
-                                <p>
-                                    nos installations modernes nous permettent
-                                    de répondre
-                                    efficacement aux besoins du marché.
-                                </p>
-                            </li>
-                            <li>
-                                <strong>Un engagement envers l’innovation :</strong>
-                                <p>
-                                    nous cherchons constamment à
-                                    améliorer nos recettes
-                                    et nos procédés pour rester à la pointe de l'industrie.
-                                </p>
-                            </li>
-                        </ul>
-                        <p style="color: #50A702">
-                            Rejoignez-nous dans notre quête pour offrir le meilleur des noix et cacahuètes enrobées, et
-                            laissez-vous séduire par la <strong>qualité supérieure</strong> et le
-                            <strong>savoir-faire</strong> d'AL
-                            AWAFY.
-                        </p>
+                        {!! $about->sec2_p3 ?? '' !!}
                     </div>
                 </div>
             </div>
@@ -123,50 +77,12 @@
                     <div class="paraFont ">
 
                         <div class="missionBtn1 my-5">
-                            <h2><span class="fs7">Notre </span> <span class="fs8">Mission</span> </h2>
+                            <h2><span class="fs7">{{ $about->sec3_h1 ?? '' }}</span> <span
+                                    class="fs8">{{ $about->sec3_h2 ?? '' }}</span> </h2>
                         </div>
                         <div class="inlineContent listStyles">
 
-                            <p>
-                                Chez <strong>AL AWAFY</strong>, notre mission est de célébrer la nature en offrant des
-                                fruits secs de la plus
-                                haute qualité, cultivés avec soin et respect. Nous nous engageons à :
-                            </p>
-                            <ol class="my-4">
-                                <li>
-                                    <strong>Promouvoir la santé :</strong> <p>
-                                        Nos fruits secs sont une source naturelle de
-                                        nutriments,
-                                        parfaits pour un mode de vie sain. Nous croyons en l'importance d'une alimentation
-                                        équilibrée et savoureuse.
-                                    </p>
-                                </li>
-                                <li>
-                                    <strong>Innovation et qualité :</strong>
-                                    <p>
-
-                                        Nous travaillons sans relâche pour
-                                        améliorer
-                                        nos produits et
-                                        proposer des saveurs uniques. Notre objectif est de vous offrir des fruits secs qui
-                                        éveillent vos sens et enrichissent vos recettes.
-                                    </p>
-                                </li>
-                                <li>
-                                    <strong> Éducation et sensibilisation :</strong>
-                                    <p>
-
-                                        Nous voulons partager notre passion
-                                        pour les fruits
-                                        secs et éduquer nos clients sur leurs bienfaits. Informer, inspirer et encourager
-                                        des
-                                        choix sains font partie intégrante de notre engagement.
-                                    </p>
-                                </li>
-                            </ol>
-                            <p style="color: #50A702">Ensemble, cultivons une passion pour la nature et une alimentation saine
-                                !
-                            </p>
+                            {!! $about->sec3_p1 ?? '' !!}
                         </div>
 
 
@@ -174,73 +90,24 @@
                     <div class="ourVisionCont paraFont">
 
                         <div class="missionBtn1 my-5" style="padding-right: 100px;">
-                            <h2><span class="fs7">Notre </span> <span class="fs8">Vision</span> </h2>
+                            <h2><span class="fs7">{{ $about->sec3_h3 ?? '' }} </span> <span
+                                    class="fs8">{{ $about->sec3_h4 ?? '' }}</span> </h2>
                         </div>
+                        {!! $about->sec3_p2 ?? '' !!}
 
-                        <p class="w-100">
-                            Chez <strong>ALAWAFY</strong>, notre vision est de devenir un leader reconnu dans le domaine
-                            des fruits secs,
-                            en redéfinissant la façon dont les consommateurs perçoivent et consomment ces produits
-                            naturels. Nous aspirons à :
-                        </p>
-                        <ol class="inlineContent listStyles">
-                            <li>
-                                <strong>Innover dans l’offre :</strong>
-                                <p>
-
-                                    Être à l’avant-garde des tendances alimentaires
-                                    en proposant
-                                    des produits innovants, savoureux et bénéfiques pour la santé.
-                                </p>
-                            </li>
-                            <li>
-                                <strong>Promouvoir un mode de vie durable :</strong>
-                                <p>
-
-                                    Inspirer une communauté engagée
-                                    autour de la
-                                    consommation responsable et durable, en favorisant les pratiques agricoles respectueuses
-                                    de l’environnement.
-                                </p>
-                            </li>
-                            <li>
-                                <strong> Renforcer les liens avec nos partenaires :</strong>
-                                <p>
-
-                                    Travailler en étroite
-                                    collaboration avec
-                                    nos producteurs locaux pour garantir une chaîne d'approvisionnement équitable et
-                                    transparente, soutenant ainsi les économies locales.
-                                </p>
-                            </li>
-                            <li>
-                                <strong> Éduquer et inspirer :</strong><p>
-                                    Devenir une référence en matière de
-                                    sensibilisation aux
-                                    bienfaits des fruits secs, en partageant des connaissances et des recettes qui
-                                    encouragent des choix alimentaires sains.
-                                </p>
-                            </li>
-                            <li>
-                                <strong>Construire un avenir meilleur :</strong>
-                                <p>
-
-                                    Contribuer à un monde où la santé, la
-                                    durabilité et
-                                    la qualité de vie sont au cœur des préoccupations alimentaires.
-                                </p>
-                            </li>
-                        </ol>
-                        <p class="w-100" style="color: #50A702">Ensemble, faisons de chaque bouchée de nos fruits secs une étape vers un avenir plus sain et
-                            durable.</p>
                     </div>
 
                     <div class="d-flex justify-content-center ourVisionPeanuts">
 
-                        <img class="img-fluid w-100"
-                            style="object-fit: contain;     object-fit: contain;
-                        max-width: 600px !important;"
-                            src="{{ asset('assets/images/items/peanuts.png') }}" alt="">
+                        @if ($about && $about->sec3_image)
+                            <img class="img-fluid w-100"
+                                style="object-fit: contain; object-fit: contain; max-width: 600px !important;"
+                                src="{{ asset('storage/' . $about->sec3_image) }}" alt="">
+                        @else
+                            <img class="img-fluid w-100"
+                                style="object-fit: contain; object-fit: contain; max-width: 600px !important;"
+                                src="{{ asset('assets/images/items/peanuts.png') }}" alt="">
+                        @endif
                     </div>
 
                 </div>
