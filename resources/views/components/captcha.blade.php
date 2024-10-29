22d2df93772f6dcd30c65df26d312af99cedd7c9
FarazPeerzada <162085198+FarazPeerzada@users.noreply.github.com>
2024-10-29 15:40:22 +0500

    Update captcha.blade.php

--- resources/views/components/captcha.blade.php
+++ resources/views/components/captcha.blade.php
@@ -1,5 +1,5 @@
 <div class="captchaDiv d-flex justify-content-start">
-    <div class="cf-turnstile" data-sitekey="1x00000000000000000000AA" id="cloud_flare_id_1"
+    <div class="cf-turnstile" data-sitekey="0x4AAAAAAAvIlPZvnIJMfP2Z" id="cloud_flare_id_1"
         data-callback="javascriptCallback"></div>
     <div id="error_message" class="text-danger fw-bold"></div>
 </div>
